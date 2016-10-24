432dd596ae0879f8a1be902358c6a677a7dc204c
spencerhooks <spencerhooks@gmail.com>
2016-10-23 21:25:54 -0700

    uncommented stuff

--- sunrise.py
+++ sunrise.py
@@ -18,17 +18,17 @@ def switch_message(client, userdata, msg):
     onoff_state = msg.payload
     client.publish("home/bedroom/sunrise", payload=msg.payload, retain=True) # publish to state topic to confirm the message was received
     if (msg.payload == "ON"): # flash the light green to acknowledge the ON message was recieved
-        # bulb.flash(duration=3, interval=1, wrgb_color='0000FF00')
+        bulb.flash(duration=3, interval=1, wrgb_color='0000FF00')
         print "Sunrise ON"
         sys.stdout.flush() # flush the output buffer so the output is immediately sent (useful when redirecting)
     elif (msg.payload == "OFF"): # flash the light red to acknowldge the OFF message was received
-        # bulb.flash(duration=3, interval=1, wrgb_color='00FF0000')
+        bulb.flash(duration=3, interval=1, wrgb_color='00FF0000')
         print "Sunrise OFF"
         sys.stdout.flush() # flush the output buffer so the output is immediately sent (useful when redirecting)
 
 def schedule_message(client, userdata, msg):
     if (onoff_state == "ON" and msg.payload == "GO"):
-        # bulb.sunrise(sunrise_length=1800)
+        bulb.sunrise(sunrise_length=1800)
         client.publish("home/bedroom/sunrise/scheduler", payload="STOP", retain=True) # publish to scheuler topic to turn off again
 
 
