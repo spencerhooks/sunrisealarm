7912e7134ecd5c7aed69977359bb7674a270e064
spencerhooks <spencerhooks@gmail.com>
2017-01-01 07:49:16 -0800

    python 3 compatibility

--- streamgen/tester.py
+++ streamgen/tester.py
@@ -14,10 +14,10 @@ p = Player()
 
 p.play(duration=5)
 
-print "playing stuff"
-print p.is_playing()
+print ("playing stuff")
+print (p.is_playing())
 
 time.sleep(6)
 # p.stop()
 
-print p.is_playing()
+print (p.is_playing())
