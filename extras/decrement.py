c2abfac61c7d31910212e93292b8880d9efac5cb
shooks <spencerhooks@gmail.com>
2017-01-23 14:12:00 -0800

    trying more decrementing

--- extras/decrement.py
+++ extras/decrement.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python
 
-a = 255
-b = 150
+from __future__ import division
 
-c = a/b+1 if a%b>0 else a/b
+#
+# a = 255
+# b = 150
+#
+# c = a/b+1 if a%b>0 else a/b
+#
+# for i in range(b):
+#     a = max(0, a-c)
+#     print(a)
 
-for i in range(b):
-    a = max(0, a-c)
-    print(a)
+
+foo = 150
+bar = 120
+
+delta = foo / bar
+
+count = 0
+while foo > 0:
+  count += 1
+  foo -= delta
+  print(int(round(max(0, foo))))
+
+print count-1
